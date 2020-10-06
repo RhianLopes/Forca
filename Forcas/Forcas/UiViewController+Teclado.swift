4468d089dd93e05484a9bfd25575a8ad9885c7b1
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-06 19:09:54 -0300

    feat: introduce initial swift project

---
+++ Forcas/Forcas/UiViewController+Teclado.swift
@@ -0,0 +1,16 @@
+//
+//  UiViewController+Teclado.swift
+//  Forcas
+//
+//  Created by Rhian Lopes da Costa on 06/10/20.
+//
+
+import UIKit
+
+class extension UIViewController {
+    
+    func dispensarTeclado() {
+        view.endEditing(true)
+    }
+    
+}
---
+++ Forcas/Forcas/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Forcas
+//
+//  Created by Rhian Lopes da Costa on 06/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toqueTecladoPronto(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBOutlet weak var bonecoImageView: UIImageView!
+    
+    var indice: Int = 1
+    
+    @IBAction func toqueBotaoRecomecar(_ sender: Any) {
+        indice += 1
+        
+        UIView.transition(
+            with: bonecoImageView,
+            duration: 0.1,
+            options: .transitionCrossDissolve,
+            animations: {
+                self.bonecoImageView.image = UIImage(named: "bonecao_fase_\(self.indice)")
+            },
+            completion: nil)
+        
+        
+        if indice == 5 {
+            indice = 0
+        }
+    }
+    
+}
+
